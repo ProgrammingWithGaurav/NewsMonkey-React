5694ecc0439c974835775d3030bf3a1e386636fb
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-10-23 16:01:00 +0530

    converting class based component to function based component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
-export class Navbar extends Component {
-    render() {
+const Navbar = (props) => {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -30,7 +29,6 @@ export class Navbar extends Component {
                 </nav>
             </div>
         )
-    }
 }
 
 export default Navbar
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,8 +1,6 @@
-import React, { Component } from 'react'
-
-export class NewsItem extends Component {
-    render() {
-        let { title, description, imageUrl, newsUrl, author, date, source } = this.props
+import React from 'react'
+const NewsItem = (props) => {
+        let { title, description, imageUrl, newsUrl, author, date, source } = props
         return (
             <div className="my-3">
                 <div className="card">
@@ -19,7 +17,6 @@ export class NewsItem extends Component {
                 </div>
             </div>
         )
-    }
 }
 
 export default NewsItem
